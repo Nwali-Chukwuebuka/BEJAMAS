9e1b1023436b156628f118e4a5b5cf553303dd7d
Nwali_Chukwuebuka_Emmanuel <Nwalichukwuebuka22@gmail.com>
2024-03-01 07:15:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function check() {
+    document.getElementById("people").checked = true;
+    document.getElementById("premium").checked = true;
+    document.getElementById("pets").checked = true;
+    document.getElementById("foods").checked = true;
+    document.getElementById("landmarks").checked = true;
+    document.getElementById("cities").checked = true;
+    document.getElementById("nature").checked = true;
+}
